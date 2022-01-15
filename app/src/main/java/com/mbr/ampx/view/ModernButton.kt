bdd43f1c6488811c489e07cfb0a609af2432dcc3
Mladen <abdiel1207@gmail.com>
2022-01-15 17:29:00 +0100

    Created main layout, modern button and added images.

--- app/src/main/java/com/mbr/ampx/view/ModernButton.kt
+++ app/src/main/java/com/mbr/ampx/view/ModernButton.kt
@@ -1,14 +1,9 @@
 package com.mbr.ampx.view
 
 import android.content.Context
-import android.content.res.Resources
-import android.graphics.PorterDuff
+import android.graphics.*
 import android.util.AttributeSet
-import android.view.LayoutInflater
 import android.view.View
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import com.mbr.ampx.R
 
 interface IModernButtonListener {
@@ -16,105 +11,191 @@ interface IModernButtonListener {
     fun onButtonLongClick(button: ModernButton)
 }
 
-class ModernButton : LinearLayout, View.OnClickListener, View.OnLongClickListener {
+class ModernButton : View, View.OnClickListener, View.OnLongClickListener {
 
-    private lateinit var backgroundView: ImageView
-    private lateinit var activeView: ImageView
-    private lateinit var textView: TextView
-    private var imageAlwaysVisible = false
-    private var active = false
+    private var bounds = RectF()
+    private var centerX: Float = 0f
+    private var centerY: Float = 0f
+    private var diameter: Float = 0f
 
-    private var imageActiveResourceId = 0
+    private var active: Boolean = false
+    private lateinit var paint: Paint
+
+    private lateinit var textPaint: Paint
+    private var text: String? = ""
+    private var textNormal: String? = ""
+    private var textActive: String? = ""
+    private var textSize: Float = 0f
+    private var textWidth: Float = 0f
+    private var textWidthHalf: Float = 0f
+    private var textHeight: Float = 0f
+    private var textHeightHalf: Float = 0f
+
+    private var circleToTextDistance: Float = 24f
+
+    private lateinit var bitmapPaint: Paint
+    private var bitmap: Bitmap? = null
+    private var normalBitmap: Bitmap? = null
+    private var activeBitmap: Bitmap? = null
+
+
+
 
     var listener: IModernButtonListener? = null
 
-
     constructor(context: Context) : super(context) {
-        initialize(context, null)
+        initialize(context, null, 0)
     }
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-        initialize(context, attrs)
+        initialize(context, attrs, 0)
     }
 
     constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
-        initialize(context, attrs)
+        initialize(context, attrs, defStyle)
     }
 
-    private fun initialize(context: Context, attrs: AttributeSet?) {
+    private fun initialize(context: Context, attrs: AttributeSet?, defStyle: Int) {
 
-        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        inflater.inflate(R.layout.view_modern_button, this, true)
+        loadAttributes(context, attrs, defStyle)
 
-        backgroundView = findViewById(R.id.imageViewButtonBackground)
-        activeView = findViewById(R.id.imageViewButtonActive)
         setOnClickListener(this)
         setOnLongClickListener(this)
-        textView = findViewById(R.id.modernButtonTextView)
+/*
+        testPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+        testPaint.style = Paint.Style.STROKE
+        testPaint.color = context.getColor(R.color.colorGradientStart)
+        testPaint.strokeWidth = 4f
+        testPaint.strokeCap = Paint.Cap.ROUND
+*/
 
-        active = false
+        paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        paint.style = Paint.Style.FILL_AND_STROKE
+        paint.color = context.getColor(R.color.colorUnderlayCircle)
+        paint.strokeWidth = 4f
+        paint.strokeCap = Paint.Cap.ROUND
 
+        textPaint = Paint(Paint.LINEAR_TEXT_FLAG or Paint.ANTI_ALIAS_FLAG)
+        textPaint.color = context.getColor(R.color.colorText)
+        textPaint.style = Paint.Style.FILL_AND_STROKE
+        textPaint.textSize = textSize
+        textPaint.textAlign = Paint.Align.LEFT
+        textPaint.isElegantTextHeight = true
+        textPaint.typeface = Typeface.DEFAULT
+        //textPaint.typeface = ResourcesCompat.getFont(context, R.font.orbitron_regular)
+        //textPaint.typeface = Typeface.createFromAsset(context.assets, "font/orbitron_medium.TTF")
+
+        bitmapPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+        bitmapPaint.style = Paint.Style.FILL
+
+        adjustActive()
+    }
+
+    private fun loadAttributes(context: Context, attrs: AttributeSet?, defStyle: Int) {
         attrs?.let {
             val a = context.obtainStyledAttributes(it, R.styleable.ModernButton, 0, 0)
 
-            val text = a.getString(R.styleable.ModernButton_modernText)
-            if (text != null) {
-                textView.text = text
+            active = a.getBoolean(R.styleable.ModernButton_modernButtonActive, false)
+            textNormal = a.getString(R.styleable.ModernButton_modernButtonNormalText)
+            textActive = a.getString(R.styleable.ModernButton_modernButtonActiveText)
+            textSize = a.getDimension(R.styleable.ModernButton_modernButtonTextSize, 0f)
+            var image = a.getResourceId(R.styleable.ModernButton_modernButtonNormalImage, 0)
+            if (image != 0) {
+                normalBitmap = BitmapFactory.decodeResource(resources, image)
             }
-
-            val topPadding = a.getDimension(R.styleable.ModernButton_modernTextViewTopPadding, 0F)
-            if (topPadding != 0F) {
-                val params = textView.layoutParams as MarginLayoutParams
-                params.setMargins(0, topPadding.toInt(), 0, 0)
-                textView.layoutParams = params
-            }
-
-            val imageBackground = a.getResourceId(R.styleable.ModernButton_modernImageBackground, -1)
-            if (imageBackground != -1) {
-                backgroundView.setBackgroundResource(imageBackground)
-            }
-
-            imageActiveResourceId = a.getResourceId(R.styleable.ModernButton_modernImageActive, -1)
-            activeView.visibility = GONE
-            if (imageActiveResourceId != -1) {
-                activeView.setImageResource(imageActiveResourceId)
-            }
-
-            imageAlwaysVisible = a.getBoolean(R.styleable.ModernButton_modernImageAlwaysVisible, false)
-            if (imageAlwaysVisible) {
-                activeView.visibility = VISIBLE
-            }
-
-            val tintActive = a.getBoolean(R.styleable.ModernButton_modernTintActiveImage, true)
-            if (tintActive) {
-                activeView.setColorFilter(resources.getColor(R.color.main), PorterDuff.Mode.MULTIPLY)
+            image = a.getResourceId(R.styleable.ModernButton_modernButtonActiveImage, 0)
+            if (image != 0) {
+                activeBitmap = BitmapFactory.decodeResource(resources, image)
             }
 
             a.recycle()
         }
     }
 
-    fun setText(text: String) { textView.text = text }
-
     fun setActive(active: Boolean) {
         this.active = active
-
-        if (imageAlwaysVisible) {
-            activeView.visibility = VISIBLE
-            return
-        }
-
-        if (active) {
-            activeView.visibility = VISIBLE
-        } else {
-            activeView.visibility = GONE
-        }
+        adjustActive()
     }
 
     fun toggleActive(): Boolean {
-        setActive(!active)
+        active = !active
+        adjustActive()
         return active
     }
+
+    private fun adjustActive() {
+        if (active) {
+            bitmapPaint.colorFilter = PorterDuffColorFilter(context.getColor(R.color.colorGradientEnd), PorterDuff.Mode.MULTIPLY)
+            text = textActive
+            bitmap = if (activeBitmap != null) {
+                activeBitmap
+            } else {
+                normalBitmap
+            }
+        } else {
+            bitmapPaint.colorFilter = PorterDuffColorFilter(context.getColor(android.R.color.white), PorterDuff.Mode.MULTIPLY)
+            text = textNormal
+            bitmap = normalBitmap
+        }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val minimumWidth = paddingStart + paddingEnd
+        val w = resolveSizeAndState(minimumWidth, widthMeasureSpec, 1)
+        val minimumHeight = paddingBottom + paddingTop + textHeight.toInt() + diameter.toInt()
+        val h = resolveSizeAndState(minimumHeight, heightMeasureSpec, 1)
+        setMeasuredDimension(w, h)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        calculateDimensionValues(w, h)
+    }
+
+    private fun calculateDimensionValues(w: Int, h: Int) {
+
+        textWidth = 0f
+        textHeight = 0f
+
+        text?.let {
+            val textBounds = Rect()
+            textWidth = textPaint.measureText(it)
+            textPaint.getTextBounds(text, 0, it.length, textBounds)
+            textHeight = Math.abs(textBounds.top + textBounds.bottom).toFloat()
+        }
+
+        textWidthHalf = textWidth / 2f
+        textHeightHalf = textHeight / 2f
+
+        bounds.left = paddingLeft.toFloat()
+        bounds.top = paddingTop.toFloat()
+        bounds.bottom = h.toFloat() - paddingEnd
+        bounds.right = w.toFloat() - paddingBottom
+        centerX = (bounds.left + bounds.right) / 2f
+        if (text == null || text == "") {
+            centerY = (bounds.top + bounds.bottom) / 2f
+            diameter = Math.min(bounds.right - bounds.left, bounds.bottom - bounds.top) / 2f
+        } else {
+            centerY = (bounds.top + bounds.bottom - textHeight - circleToTextDistance) / 2f
+            diameter = Math.min(bounds.right - bounds.left, bounds.bottom - bounds.top - (textHeight * 2f) - circleToTextDistance) / 2f
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        //canvas.drawRect(bounds, testPaint)
+        canvas.drawCircle(centerX, centerY, diameter, paint)
+
+        text?.let {
+            canvas.drawText(it, centerX - textWidthHalf, centerY + diameter + textHeight + circleToTextDistance, textPaint)
+        }
+
+        bitmap?.let {
+            canvas.drawBitmap(it, centerX - it.width / 2f, centerY - it.height / 2f, bitmapPaint)
+        }
+
+    }
+
 
     override fun onClick(view: View?) {
         listener?.onButtonClick(this)
